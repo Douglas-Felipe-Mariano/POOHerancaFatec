077f2215b555b0d06c9a41fc342392955cd7ffb7
Douglas Felipe Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2025-09-02 00:37:39 +0000

    Introdução a Array

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+public class Main{
+    public static void main(String[] args) {
+        Tarefa[] tarefas = new Tarefa[5];
+
+        tarefas[0] = new Tarefa(0, "Teste", false);
+        tarefas[4] = new Tarefa(3, "Novo Teste", false);
+
+
+        System.out.println(tarefas[0].getDescricao());
+        System.out.println(tarefas[4].getDescricao());
+    }
+}   
